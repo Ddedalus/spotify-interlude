44254b35f1c24adb0bbcf3fd61ad042eb39caace
Hubert Bereś <h.beres@hotmail.com>
2022-08-05 19:26:28 +0100

    Register logging callbacks

--- interlude/main.py
+++ interlude/main.py
@@ -12,10 +12,24 @@ foreground_process_names = ["chrome.exe", "firefox.exe", "Spotify.exe"]
 sessions: Dict[int, AudioSession] = {}
 
 
-def unregister_callbacks(sessions: Iterable[AudioSession]):
+def _unregister_callbacks(sessions: Iterable[AudioSession]):
     for s in sessions:
-        print(f"Unregistered callback for {s.Process and s.Process.name()}")
         s.unregister_notification()
+        print(f"Unregistered callback for {s.Process and s.Process.name()}")
+
+
+class StateLoggingCallback(AudioSessionEvents):
+    def __init__(self, process_name: str = "") -> None:
+        super().__init__()
+        self.process_name = process_name
+
+    def on_state_changed(self, new_state, new_state_id):
+        print(f"{self.process_name} changed state to {new_state}")
+
+    def on_session_disconnected(self, disconnect_reason, disconnect_reason_id):
+        print(
+            f"{self.process_name} session was disconnected because {disconnect_reason}"
+        )
 
 
 def discover_foreground_sessions(
@@ -37,20 +51,19 @@ def discover_foreground_sessions(
 
     # Delete sessions which have sinde disappeared
     deleted = {pid: s for pid, s in sessions.items() if pid not in discovered_sessions}
-    unregister_callbacks(deleted.values())
+    _unregister_callbacks(deleted.values())
     for pid in deleted:
         sessions.pop(pid)
 
     new = {pid: s for pid, s in discovered_sessions.items() if pid not in sessions}
     for pid, session in new.items():
-        name = session.Process and session.Process.name()
-        print(f"TODO: register callback for {name}")
+        name = session.Process and session.Process.name() or "unknown"
+        callback = StateLoggingCallback(name)
+        session.register_notification(callback)
+        print(f"Registered callback for {name}")
         sessions[pid] = session
 
-    print(f"Found {len(sessions)} sessions")
-    for session in sessions.values():
-        status = "active" if session.State else "inactive"
-        print(f"{session.Process and session.Process.name()} is {status}")
+    print(f"Tracking {len(sessions)} audio sessions")
 
     # check again in a moment
     scheduler.enter(3, 5, discover_foreground_sessions, (scheduler, sessions))
@@ -64,4 +77,4 @@ if __name__ == "__main__":
     except KeyboardInterrupt:
         print("Keyboard Interrupt, cleaning up...")
     finally:
-        unregister_callbacks(sessions.values())
+        _unregister_callbacks(sessions.values())
